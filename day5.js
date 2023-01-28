a7d84c16bb90d25ab9cd69f8b7c4f0ce698f4c9a
Archit R Srivastava <99673217+archit-1203@users.noreply.github.com>
2023-01-28 18:07:51 +0530

    Add files via upload

--- day5.js
+++ day5.js
@@ -19,3 +19,28 @@ document.getElementById("start").addEventListener("click",function handleClick()
 document.getElementById("stop").addEventListener("click",function handleClick(){
     document.removeEventListener("mouseover",handleMouseOver)
 }) ;
+
+// document.addEventListener("keyup",function(e){
+//     console.log(e.key);
+// })
+document.addEventListener("keypress",function(e){
+    console.log("keypress");
+    console.log(e.key);
+    if(e.key == 2){
+        e.preventDefault();
+    }
+})
+document.addEventListener("keyup",function(e){
+    console.log("keyup");
+    console.log(e.key);
+    if(e.key == 2){
+        e.preventDefault();
+    }
+})
+document.addEventListener("keydown",function(e){
+    console.log("keydown");
+    console.log(e.key);
+    if(e.key == 2){
+        e.preventDefault();
+    }
+})
